5d933f980d1be57471b8e42a7338c1dad03c9d48
vaaandark <vaaandark@gmail.com>
2022-08-26 02:45:38 +0800

    添加测试脚本

---
+++ scripts/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+  echo "usage: test.sh {regexp} {str}"
+  exit 1
+fi
+
+regexp=$1
+str=$2
+
+(
+  cd "$(dirname "$0")" || exit 1
+
+  cd ../target/ || exit 1
+
+  name="$(mktemp finalXXX)"
+
+  cat << EOF > "$name".c
+#include <stdio.h>
+#include <string.h>
+
+EOF
+
+  ./reg2dfa "$regexp" >> "$name".c
+
+  cat << EOF >> "$name".c
+int main(int argc, char *argv[]) {
+    char reg[128];
+    int len, success;
+    strcpy(reg, argv[1]);
+    if (match(&len, &success, reg)) {
+        printf("match for %d chars\n", len);
+    } else {
+        printf("not match\n");
+    }
+    return 0;
+}
+EOF
+
+  gcc "$name".c -o "$name"
+  ./"$name" "$str"
+  rm "$name" "$name".c
+)
+
